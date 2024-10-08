76533b128e0fec245a14e875a6e67fd4b370781e
saba ali <sabaali3201@gmail.com>
2024-10-08 23:17:29 +0500

    added migrations

--- migrations/2_deploy_bank_account.js
+++ migrations/2_deploy_bank_account.js
@@ -1,8 +1,5 @@
-// Import the contract
 const BankAccount = artifacts.require("BankAccount");
 
-// Export the migration function
 module.exports = function (deployer) {
-    // Deploy the contract
     deployer.deploy(BankAccount);
 };
